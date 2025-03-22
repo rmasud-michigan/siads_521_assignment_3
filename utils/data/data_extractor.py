a8f31777db390facb82e064eaf5f2cd0d52842bf
rmasud-umichigan <rmasud@umich.edu>
2025-03-22 18:33:56 -0500

    more data changes

--- utils/data/data_extractor.py
+++
@@ -1,6 +0,0 @@
-import requests
-import os
-import pandas as pd
-
-#const_src_url = "https://data.cityofchicago.org/api/views/hhkd-xvj4/rows.csv?accessType=DOWNLOAD"
-#const_storage_file ="assets/data/chicago_red_light_cameras.csv"
